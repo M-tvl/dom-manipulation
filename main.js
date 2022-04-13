3541b3d064fa28816b7c22227a1b24d733d4f9e2
Mohammad Tavallaee <m.tvl1364@gmail.com>
2022-04-13 13:02:47 +0300

    assignment

---
+++ main.js
@@ -0,0 +1,13 @@
+var parag = document.createElement("p");
+parag.innerText = "Hey I'm red!";
+document.body.appendChild(parag);
+parag.style.color = "red";
+
+var txt = document.createElement("h3");
+txt.innerText = "I'm a blue h3!";
+document.body.appendChild(txt);
+txt.style.color = "blue";
+
+
+
+
